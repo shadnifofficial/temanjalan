22faa8a117467e922ab9fa7ddfd23cbfcdef9fb1
shadnifofficial <84317567+shadnifofficial@users.noreply.github.com>
2024-08-23 11:49:18 +0900

    Add files via upload

---
+++ WEB/scipt.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger')
+const navLinks = document.getElementById('nav-links')
+
+hamburger.addEventListener('click', function(){
+    navLinks.classList.toggle('active')
+})
